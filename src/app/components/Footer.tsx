614b1a67775e307b4dda8f25550ecd4cd4ccd2b2
imtithal <halaloimtithal@gmail.com>
2024-04-22 03:02:30 +0300

    adding footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#f1f4ff] border-l-transparent border-r-transparent text-white mt-5">
+      <div className="container p-12 flex justify-between">
+        <span>I <span className='text-[#03b9f4]'>H</span></span>
+        <p className="text-slate-50">All rights reserved @Imtithal Halalo.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
