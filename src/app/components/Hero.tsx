9f51ea63736aa3861a136d2bdee942cb6d56f319
imtithal <halaloimtithal@gmail.com>
2024-04-24 21:38:16 +0300

    -

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-5xl font-extrabold">
-                    Hello, I'm {' '}
+                    Hello, I am {' '}
                     <br></br>
                     <span className='text-[#03b9f4]'>
                         <TypeAnimation
