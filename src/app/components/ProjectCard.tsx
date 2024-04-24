88ead83e75d0219895cab96240411152677b0fd0
imtithal <halaloimtithal@gmail.com>
2024-04-24 21:30:31 +0300

    adding video modal

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,6 +2,7 @@ import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/solid'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import VideosModal from './VideosModal'
 
 interface IProjectCard {
     imageUrl: string
@@ -12,6 +13,9 @@ interface IProjectCard {
 }
 
 const ProjectCard = ({ imageUrl, title, description, github, video }: IProjectCard) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+
     return (
         <div>
             <div
@@ -27,23 +31,31 @@ const ProjectCard = ({ imageUrl, title, description, github, video }: IProjectCa
             <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
                 <Link
                     href={github}
+                    target='_blank'
                     className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
                 >
                     <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
                 </Link>
-                <Link
-                    href={video}
-                    target='_blank'
+                <button
+                    onClick={handleOpen}
                     className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
                 >
                     <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-                </Link>
+                </button>
             </div>
         </div>
+
         <div className="text-white rounded-b-xl mt-3 bg-[#e4e4e49b] py-6 px-4">
             <h5 className="text-xl font-semibold mb-2">{title}</h5>
             <p className="text-[#010101]">{description}</p>
         </div>
+        <VideosModal
+            title={title}
+            description={description}
+            videoUrl={video}
+            open={open}
+            setOpen={setOpen}
+        />
       </div>
     )
 }
---
+++ src/app/components/VideosModal.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 600,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  height: 600,
+  p: 4,
+  borderRadius: '20px'
+};
+
+interface IVideosModal {
+    title: string
+    videoUrl: string
+    description: string
+    open: boolean
+    setOpen: any
+}
+const VideosModal = ({ title, description, videoUrl, open, setOpen }: IVideosModal) => {
+  const handleClose = () => setOpen(false);
+
+  return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <div className='flex flex-row justify-between'>
+                    <div>
+                        <Typography id="modal-modal-title"  className=' text-black' variant="h6" component="h2">
+                            {title}
+                        </Typography>
+                    </div>
+                    <div>
+                        <button onClick={handleClose} className='text-black flex items-center px-3 py-2 rounded hover:text-black'>
+                            <XMarkIcon className='h-5 w-5'/>
+                        </button>
+                    </div>
+                </div>
+                <Typography id="modal-modal-description" className=' text-black' sx={{ mt: 2 }}>
+                    {description}
+                </Typography>
+                <div className="mt-6">
+                    <div className='embed-responsive embed-responsive-16by9'>
+                        <iframe width='540' height='315' className='embed-responsive-item' src={videoUrl} allowFullScreen></iframe>
+                    </div>
+                </div>
+            </Box>
+        </Modal>
+  );
+}
+
+export default VideosModal;
