b08f8b79069f36c84a4f7c82f0790987783b85ac
imtithal <halaloimtithal@gmail.com>
2024-04-24 21:30:52 +0300

    moving projects to separate file

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,60 +3,9 @@ import React, { useRef, useState } from 'react'
 import ProjectCard from './ProjectCard'
 import { motion, useInView } from "framer-motion";
 import ProjectTag from './ProjectTag';
+import { projects } from '../constants/projects';
 
-const projects = [
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'Fitnest',
-        description: `Developed a Mobile App where the user's data is collected and
-        connected to fitness trainers. Provides a full vision of their progress
-        using charts and graphs. Developed using React Native, Laravel, MySQL, and Firebase.`,
-        github: 'https://github.com/imtithalhalalo/fitnest',
-        video: '',
-        tag: ["All", "Mobile"],
-    },
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'Coffee Shop',
-        description: 'Coffee Shop Management System with Laravel MySQL and Bootstrap and Tailwind CSS.',
-        github: 'https://github.com/imtithalhalalo/coffee-shop',
-        video: '',
-        tag: ["All", "Web"],
-    },
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'Lets Write',
-        description: 'MERN Story Sharing Application where you can share your own story.',
-        github: 'https://github.com/imtithalhalalo/LetsWrite',
-        video: '',
-        tag: ["All", "Web"],
-    },
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'Alpha Store',
-        description: `A store application built with TypeScript, utilizing Redux Toolkit for state management. 
-        It includes features such as managing data with Redux Toolkit, implementing search, filter, and sorting functionalities, and performing cart actions.`,
-        github: 'https://github.com/imtithalhalalo/AlphaStore',
-        video: '',
-        tag: ["All", "Web"],
-    },
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'To do list',
-        description: 'This ToDo List application provides a simple and intuitive way to manage your tasks efficiently. Built with Redux for state management, it offers powerful features such as search functionality and filters for organizing tasks based on completion status.',
-        github: 'https://github.com/imtithalhalalo/todolist-redux-tailwindCSS',
-        video: '',
-        tag: ["All", "Web"],
-    },
-    {
-        imageUrl: '/images/projects/memory-game.png',
-        title: 'Memory Game',
-        description: 'Memory Card Game! This is a simple web-based game where you need to match pairs of cards.',
-        github: 'https://github.com/imtithalhalalo/memory-game-reactjs',
-        video: '',
-        tag: ["All", "Web"],
-    },
-]
+
 const Projects = () => {
     const [tag, setTag] = useState("All");
     const ref = useRef(null);
---
+++ src/app/constants/projects.ts
@@ -0,0 +1,53 @@
+export const projects = [
+    {
+        imageUrl: '/images/projects/fitnest.png',
+        title: 'Fitnest',
+        description: `Developed a Mobile App where the user's data is collected and
+        connected to fitness trainers. Provides a full vision of their progress
+        using charts and graphs. Developed using React Native, Laravel, MySQL, and Firebase.`,
+        github: 'https://github.com/imtithalhalalo/fitnest',
+        video: 'https://www.youtube.com/embed/CP7KoS9O430?si=oV7sXPNmxqJfZw_2',
+        tag: ["All", "Mobile"],
+    },
+    {
+        imageUrl: '/images/projects/coffee-shop.png',
+        title: 'Coffee Shop',
+        description: 'Coffee Shop Management System with Laravel MySQL and Bootstrap and Tailwind CSS.',
+        github: 'https://github.com/imtithalhalalo/coffee-shop',
+        video: 'https://www.youtube.com/embed/l0n4bnStWMQ?si=KiWkJ6lCJPjLEEAE',
+        tag: ["All", "Web"],
+    },
+    {
+        imageUrl: '/images/projects/letswrite.png',
+        title: 'Lets Write',
+        description: 'MERN Story Sharing Application where you can share your own story.',
+        github: 'https://github.com/imtithalhalalo/LetsWrite',
+        video: 'https://www.youtube.com/embed/87FFIAL9WZc?si=8n7bRhpCuVwAGQ3r',
+        tag: ["All", "Web"],
+    },
+    {
+        imageUrl: '/images/projects/alphastore.png',
+        title: 'Alpha Store',
+        description: `A store application built with TypeScript, utilizing Redux Toolkit for state management. 
+        It includes features such as managing data with Redux Toolkit, implementing search, filter, and sorting functionalities, and performing cart actions.`,
+        github: 'https://github.com/imtithalhalalo/AlphaStore',
+        video: 'https://www.youtube.com/embed/AyoflsWlWvM?si=mGEJusvp57d3TkvR',
+        tag: ["All", "Web"],
+    },
+    {
+        imageUrl: '/images/projects/to-do-list.png',
+        title: 'To do list',
+        description: 'This ToDo List application provides a simple and intuitive way to manage your tasks efficiently. Built with Redux for state management, it offers powerful features such as search functionality and filters for organizing tasks based on completion status.',
+        github: 'https://github.com/imtithalhalalo/todolist-redux-tailwindCSS',
+        video: 'https://www.youtube.com/embed/J4u46iZdj_c?si=1EM1g4zLajdRbClI',
+        tag: ["All", "Web"],
+    },
+    {
+        imageUrl: '/images/projects/memory-game.png',
+        title: 'Memory Game',
+        description: 'Memory Card Game! This is a simple web-based game where you need to match pairs of cards.',
+        github: 'https://github.com/imtithalhalalo/memory-game-reactjs',
+        video: 'https://www.youtube.com/embed/MChyN0roI40?si=O_ppI1RX4drURxeL',
+        tag: ["All", "Web"],
+    },
+]
